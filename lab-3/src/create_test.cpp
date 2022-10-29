c4341b25841b756a39929ff454292c7402a3b90e
Alexander Drukholskii <90084795+ssForz@users.noreply.github.com>
2022-10-29 18:53:41 +0200

    Update create_test.cpp

--- lab-3/src/create_test.cpp
+++ lab-3/src/create_test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <ctime>
-#include<fstream>                    
+#include<fstream> 
+#include<vector>                   
 using namespace std;
 
 int main(int argc, char* argv[])
@@ -8,16 +9,11 @@ int main(int argc, char* argv[])
     ofstream out;
     out.open("big_massives.txt");
     srand(time(0));
-    const int array_size = 1000; // размер одномерного массива
-    int array1[array_size]; // объявление одномерного массива
+    const int array_size = 1e8; // размер одномерного массива
     for (int counter = 0; counter < array_size; counter++) {
-        array1[counter] = rand() % 50 - rand() % 50; // заполняем массив случайными значениями в диапазоне от -49 до 49 включительно
-        cout << array1[counter] << " "; // печать элементов одномерного массива array1
+        out<<rand() % 50 - rand() % 50<<endl;
     }
     
-    for (int counter = 0; counter < array_size; counter++) {
-        out<<array1[counter]<<" ";
-    }
     cout << "runtime = " << clock()/1000.0 << endl; // время работы программы                  
     return 0;
 }
