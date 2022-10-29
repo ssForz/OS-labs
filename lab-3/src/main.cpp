f0a6312dac729e4ff6eb8dd7bbaaf6121647a829
Alexander Drukholskii <90084795+ssForz@users.noreply.github.com>
2022-10-29 18:53:11 +0200

    Update main.cpp

--- lab-3/src/main.cpp
+++ lab-3/src/main.cpp
@@ -1,13 +1,16 @@
 #include<pthread.h>
 #include<iostream>
 #include <ctime> 
+#include<vector>
+#include<fstream>
+#include<chrono>
 
 using namespace std;
 
 static int minimal = 1e9;
 pthread_mutex_t mutex;
 int flag = 0;
-
+vector<int> result(100);
 
 struct arg_to_thread {
 	int* big_array;
@@ -19,6 +22,7 @@ struct arg_to_thread {
 
 void* thread_func(void *args)
 {	
+    int minimum = 1e9;
 	arg_to_thread* arguments = (arg_to_thread*) args;
 	int num_of_thread = arguments->num_of_thread;
 	int partition = arguments->partition;
@@ -26,25 +30,20 @@ void* thread_func(void *args)
 	int count_threads = arguments->count_threads;
 	int size_array = arguments->size_array;
 	int* big_array = arguments->big_array;
-		cout<<"You in thread: "<<num_of_thread<<endl;
 
 		if (num_of_thread != count_threads - 1) {
 			for (int j = num_of_thread * partition; j < num_of_thread * partition + partition; ++j) {
-				cout<<"Выполнение в потоке "<<num_of_thread<<endl;
-				pthread_mutex_lock(&mutex);
-					if (big_array[j] < minimal) {
-						minimal = big_array[j];
+					if (big_array[j] < minimum) {
+						minimum = big_array[j];
+                        result[num_of_thread] = big_array[j];
 					}
-				pthread_mutex_unlock(&mutex);
 			}	
 		} else {
 			for (int j = size_array - 1; j > size_array - partition - 1; --j) {	
-				cout<<"Выполнение в потоке "<<num_of_thread<<endl;
-				pthread_mutex_lock(&mutex);
-					if (big_array[j] < minimal) {
-						minimal = big_array[j];
-					}
-				pthread_mutex_unlock(&mutex);
+					if (big_array[j] < minimum) {
+                        minimum = big_array[j];
+                        result[num_of_thread] = big_array[j];
+                    }
 			}	
 
 		}
@@ -56,7 +55,8 @@ void* thread_func(void *args)
 
 int main(int argc, char const *argv[])
 {
-	srand(time(0));
+	
+    ifstream file("./src/big_massives.txt");
 	int count_threads;
 	cout<<"Введите количество потоков: ";
 	cin>>count_threads;
@@ -65,10 +65,13 @@ int main(int argc, char const *argv[])
 	cout<<"Введите размер массива: ";
 	cin>>size_array;
 	cout<<endl;
-	int big_array[size_array];
+	vector<int>big_array(size_array);
 	cout<<"Введите массив: "<<endl;
 	for(int i = 0; i < size_array; ++i) {
-		cin>>big_array[i];
+        string s;
+        getline(file, s);
+
+	   big_array[i] = atoi(s.c_str());
 	}
 	if (count_threads > size_array) {
 		cout<<"Количество потоков больше максимального"<<endl;
@@ -80,10 +83,11 @@ int main(int argc, char const *argv[])
 	int partition = size_array / count_threads;
 	struct arg_to_thread arg;
 	arg.partition = partition;
-	arg.big_array = big_array;
+	arg.big_array = big_array.data();
 	arg.count_threads = count_threads;
 	arg.size_array = size_array;
 
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
 	for (int i = 0; i < count_threads; i++) {
 		arg.num_of_thread = i;
 		if (i == count_threads - 1) {
@@ -103,11 +107,17 @@ int main(int argc, char const *argv[])
 	for (int i = 0; i < count_threads; ++i) {
 		pthread_join(threads[i], NULL);
 	}
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
 
 	pthread_mutex_destroy(&mutex);
-
-	cout<<"Минимальное значение: "<<minimal<<endl;
-	cout << "runtime = " << clock()/1000.0 << endl;
+    int minimum2 = 1e9;
+    for (int i = 0; i < count_threads; ++i) {
+        if (result[i] < 1e9) {
+            minimum2 = result[i];
+        }
+    }
+	cout<<"Минимальное значение: "<<minimum2<<endl;
+    cout<<chrono::duration_cast<chrono::microseconds>(end-begin).count();
 	return 0;
 
 }
